2b06eb732b5366fce9530c030dc89af07ccf261c
Budhabhushan <budhbhushan.jawale@torinit.ca>
2023-07-10 21:34:00 +0530

    some codes are moved

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,22 @@
 import React from 'react';
-import {BrowserRouter, Route, Routes,Link} from 'react-router-dom'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import './App.css';
 import Login from './pages/Login/Login'
-import Home from './pages/Home/Home';
 import Logout from './pages/LogOut/Logout';
-import NavBar from './componets/NavBar/NavBar';
 import PageNotFound from './pages/PageNotFound/PageNotFound';
-import User from './pages/User/User';
 import Songs from './pages/Songs/Songs';
 function App() {
   return (
     <>
      <BrowserRouter>
-     <NavBar/>
        <Routes>
         <Route path='/' element={<Login />}/>
         <Route path='/logout' element={<Logout/>}/>
-        <Route path='/home' element={<Home/>}/>
         <Route path='/*' element={<PageNotFound/>}/>
-        <Route path='/user/:name' element={<User/>}/>
         <Route path='/songs' element={<Songs/>}></Route>
        </Routes>
      </BrowserRouter>
     </>
-    // <div className="App">
-
-    //   <h1 className="text-3xl font-bold underline bg-yellow-200">React with Netlify CI CD PipeLine</h1>
-    //   <h1>New Feature adde</h1>
-    //   <h1>React With the files and music with old and new sone</h1>
-    // </div>
   );
 }
 
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,9 +1,28 @@
+import { useState } from "react"
+import {useDispatch} from 'react-redux'
+import './style.css'
+import { login } from "../../features/userSlice"
 const Login = () => {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
   return (
-    <div>
-      <h1>
-         Welcome to Login
-      </h1>
+    <div className="login">
+      <form className="login__form">
+        <h1>Login Here..</h1>
+        <input 
+          type="email" 
+          placeholder="Enter Email" 
+          value={email} 
+          onChange={(e) => setEmail(e.target.value)} 
+        />
+        <input 
+          type="password" 
+          placeholder="Enter Password" 
+          value={password} 
+          onChange={(e)=> setPassword(e.target.value)}
+        />
+        <button type="submit" className="submit__btn">LogIn</button>
+      </form>
     </div>
   )
 }
